b25c4f6e005cb41a7637e8a5e99cc586425c2cd6
Vivek Kumar Patel <vivekece1116@gmail.com>
2024-02-22 11:54:44 +0530

    Movie app created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Footer from "./components/Footer.js"
+function App() {
+  return (
+    <div className="App">
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
